46b19c5492bc0fe3bbae0d13736806785aa2d1ad
Andreas Pauley <apauley+github@gmail.com>
2010-08-01 20:15:24 +0200

    A small explanation of the implementation for the secant method

--- Chapter1/secant.scm
+++ Chapter1/secant.scm
@@ -1,3 +1,11 @@
+;; The Secant Method
+;; Section 1.2, p. 38 of Applied Numerical Analysis
+
+;; secant takes as input a function f and 2 initial values (x0 and x1),
+;; from which a third value is computed.
+;; The function then calls itself recursively with newly computed values,
+;; until a root of f is found.
+
 (define (secant f x0 x1)
   (define x2
     (- x1
